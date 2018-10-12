8af77c5151dfa209aaeb29839813325751cacd5c
Asgard Moreno <oscar.edmor@gmail.com>
2018-10-11 22:01:42 -0500

    googleflex1

---
+++ semana_5/dia_4/google.js
@@ -0,0 +1,19 @@
+let searchInput = document.querySelector('#InSearch')
+let searchButton = document.querySelector('#search')
+
+function validar(event){
+if(searchInput.value.match(/^ +$/)){ //^ inicio de cadena, $ fin de cadena, "espacio y +", que haya un espacio o mas
+event.defaultPrevented;
+return false;
+    }
+}
+function search (){
+    validar()
+    window.location.href=`https://www.google.com.mx/search?q=${searchInput.value}`;
+    }
+function enter(e){
+    if((e.key) == 'Enter')
+    search()
+}
+searchInput.addEventListener('keyup', enter)
+searchButton.addEventListener('click', search)
